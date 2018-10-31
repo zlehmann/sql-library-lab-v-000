9a70a9c745818f5f55ce995bec908d75b0086f6a
Zachary Lehmann <zlehmann@gmail.com>
2018-10-31 01:11:46 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name FROM authors, subgenres LEFT OUTER JOIN series ON authors.id = series.author_id WHERE series.author_id = authors.id ORDER BY authors.name AND subgenres.name;"
+  "SELECT authors.name, subgenres.name FROM authors, subgenres LEFT OUTER JOIN series ON authors.id = series.author_id ORDER BY authors.name AND subgenres.name;"
 end
 
 def select_series_title_with_most_human_characters
