2651cec1563b3d23e5abea84a3751704165f350f
Zachary Lehmann <zlehmann@gmail.com>
2018-10-31 00:27:08 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,5 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT books.title, books.year FROM series ORDER BY books.year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
