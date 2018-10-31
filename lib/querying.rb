03fb85acdc97837c130e3bd9f1dd95997677ec9a
Zachary Lehmann <zlehmann@gmail.com>
2018-10-31 01:20:22 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,5 +20,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, SUM(books) FROM characters LEFT OUTER JOIN series ON characters.series_id = series.id;"
 end
