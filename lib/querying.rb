668fa752fa8d1dc129f720a30f42b972bcaed8d3
Zachary Lehmann <zlehmann@gmail.com>
2018-10-31 00:33:06 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -3,7 +3,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT characters.name, characters.motto FROM characters WHERE (SELECT MAX(LEN(motto)) FROM characters);"
 end
 
 
