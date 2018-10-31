258a613d39a7ec93e88d808932b3a3a690d2c710
Zachary Lehmann <zlehmann@gmail.com>
2018-10-31 01:17:44 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series INNER JOIN characters ON series.id = characters.series_id WHERE characters.species = \"human\";"
+  "SELECT series.title FROM series INNER JOIN characters ON series.id = characters.series_id WHERE characters.species = \"human\" LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
