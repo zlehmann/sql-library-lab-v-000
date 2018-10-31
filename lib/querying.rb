db0500b93783a40323b65aba19571d7792c8e2c8
Zachary Lehmann <zlehmann@gmail.com>
2018-10-31 00:40:59 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT author.name, subgenres.name FROM series;"
+  "SELECT authorS.name, subgenres.name FROM series;"
 end
 
 def select_series_title_with_most_human_characters
