82ce79856067050a9a833b25c225f1283f604010
Zachary Lehmann <zlehmann@gmail.com>
2018-10-31 01:36:15 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,5 +20,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(books.id) FROM characters, books LEFT OUTER JOIN character_books ON characters.id = character_books.character_id WHERE characters.id = character_books.character_id GROUP BY characters.name;"
+  "SELECT characters.name, COUNT(books.id) FROM characters, books LEFT OUTER JOIN character_books ON characters.id = character_books.character_id WHERE characters.id = character_books.character_id;"
 end
